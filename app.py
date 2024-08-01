419a20674441bae6004f260fba3b4d9642605f20
nuzats19 <126117403+nuzats19@users.noreply.github.com>
2024-08-01 15:48:50 -0400

    web app file

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for, redirect, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('regressor_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+	return redirect(url_for('predict_weight'))
+
+@app.route('/predict_weight', methods = ['GET', 'POST'])
+def predict_weight():
+	if request.method == 'POST':
+		height = request.form['height']
+		height = float(height) / 2.54
+		weight = model.predict([[height]]) 
+		return render_template("index.html", weight = weight[0][0] / 2.205, unit = "kgs")
+	return render_template("index.html")
+
+@app.route('/api/predict_weight', methods=['POST'])
+def api_predict_weight():
+    if request.method == 'POST':
+        data = request.get_json()
+        height = data['height']
+        height = float(height) / 2.54
+        weight = model.predict([[height]])
+        return jsonify({"weight": weight[0][0] / 2.205, "unit": "kgs"})
+
+if __name__ == '__main__':
+	app.run(debug = True)
